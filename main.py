a642c01922b01431fce0230911e6597deac4b2c6
Jony <116832148+oddava@users.noreply.github.com>
2025-05-14 15:49:09 +0500

    assignments complete

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-from aiogram.utils.text_decorations import markdown_decoration
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -33,16 +32,7 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-
 
 @dp.message(F.text.regexp(r'\b\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])\b'))
 async def age_handler(message: Message) -> None:
@@ -94,10 +84,7 @@ async def handle_game(message: Message) -> None:
     await message.send_copy(message.chat.id)
 
 async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    # And the run events dispatching
     await dp.start_polling(bot)
 
 
